b3529f5b3f3f5373354e85011960ab3e48240f96
edzvonik <ev.dzvonik@gmail.com>
2023-01-02 14:57:15 +0300

    ArrayStorage class implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,55 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (size < 10000) {
+            storage[size] = r;
+            size++;
+        } else {
+            System.out.println("ArrayStorage is overflow. Please, create a new storage");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                System.arraycopy(storage, i + 1, storage, i, (size - (i + 1)));
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] all = new Resume[size];
+        System.arraycopy(storage, 0, all, 0, size);
+        return all;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
