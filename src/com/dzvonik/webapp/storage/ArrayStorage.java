3b03d6681479a55ca048b3f307590dcbdb054b44
edzvonik <ev.dzvonik@gmail.com>
2023-01-04 19:13:24 +0300

    Refactor ArrayStorage.java and move to src/com/dzvonik/webapp/storage package

--- src/com/dzvonik/webapp/storage/ArrayStorage.java
+++ src/com/dzvonik/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.dzvonik.webapp.storage;
+
+import com.dzvonik.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,34 +9,42 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
+    public void update(Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(r.getUuid())) {
+                storage[i] = r;
+            }
+        }
+    }
+
+    public void save(Resume r) {
         if (size < storage.length) {
             storage[size] = r;
             size++;
         } else {
-            System.out.println("ArrayStorage is overflow. Please, create a new storage");
+            System.out.println("com.dzvonik.webapp.storage.ArrayStorage is overflow. Please, create a new storage");
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = null;
                 System.arraycopy(storage, i + 1, storage, i, size - (i + 1));
                 size--;
@@ -44,13 +56,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[size];
         System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
