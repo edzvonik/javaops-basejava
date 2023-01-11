916e0a64f83ac29725f0ad65e82187b909e16ac1
edzvonik <ev.dzvonik@gmail.com>
2023-01-11 09:59:02 +0300

    Add SortedArrayStorage

---
+++ src/com/dzvonik/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,10 @@
+package com.dzvonik.webapp.storage;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected int exist(String uuid) {
+        return 0;
+    }
+
+}
