3742dd4292d7ba1ccde73184ffc7d2a1e08609cf
edzvonik <ev.dzvonik@gmail.com>
2023-01-11 10:55:32 +0300

    Add constructor to Resume.java and implement exist(String uuid) in SortedArrayStorage

--- src/com/dzvonik/webapp/model/Resume.java
+++ src/com/dzvonik/webapp/model/Resume.java
@@ -16,6 +16,10 @@ public class Resume {
         this.uuid = uuid;
     }
 
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/com/dzvonik/webapp/storage/SortedArrayStorage.java
+++ src/com/dzvonik/webapp/storage/SortedArrayStorage.java
@@ -1,10 +1,15 @@
 package com.dzvonik.webapp.storage;
 
+import com.dzvonik.webapp.model.Resume;
+
+import java.util.Arrays;
+
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int exist(String uuid) {
-        return 0;
+        Resume searchKey = new Resume(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
 }
