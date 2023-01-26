60c24dba0a8c59b62b477e294c28170bd76d9db3
edzvonik <ev.dzvonik@gmail.com>
2023-01-26 06:20:34 +0300

    lesson04 - practice with reflection

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -4,10 +4,15 @@ import java.lang.reflect.Field;
 
 public class MainReflection {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
         Resume resume = new Resume();
-        Field field = resume.getClass().getDeclaredFields()[0];
-        System.out.println(resume);
+        Field field = resume.getClass().getDeclaredField("uuid");
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        String newUuid = "Changed uuid";
+        System.out.println(resume.getUuid());
+        field.set(resume, newUuid);
+        System.out.println(resume.getUuid());
     }
 
 }
