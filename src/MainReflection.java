ac42b6f0403ce55a2165d25ce1aea1b1d8e81f59
edzvonik <ev.dzvonik@gmail.com>
2023-01-26 06:28:52 +0300

    Invoke toString with Reflection API

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -1,10 +1,12 @@
 import com.dzvonik.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredField("uuid");
         field.setAccessible(true);
@@ -13,6 +15,10 @@ public class MainReflection {
         System.out.println(resume.getUuid());
         field.set(resume, newUuid);
         System.out.println(resume.getUuid());
+        // TODO: Invoke toString() with Reflection API
+        Method toStringMethod = resume.getClass().getDeclaredMethod("toString");
+        String resumeString = (String) toStringMethod.invoke(resume);
+        System.out.println(resumeString);
     }
 
 }
