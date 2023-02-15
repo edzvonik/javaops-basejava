f71d8ddc7db0429f26c4d7b28058e0cf991cc35b
edzvonik <ev.dzvonik@gmail.com>
2023-02-15 05:53:02 +0300

    hw08 - recursively traverse directories

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,10 +1,28 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainFile {
 
     public static void main(String[] args) {
+        String currentDirectory = System.getProperty("user.dir");
+        try (Stream<Path> walk = Files.walk(Paths.get(currentDirectory))) {
+
+            List<String> result = walk.filter(Files::isDirectory)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+
+            result.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         String filePath = ".\\.gitignore";
 
         File file = new File(filePath);
