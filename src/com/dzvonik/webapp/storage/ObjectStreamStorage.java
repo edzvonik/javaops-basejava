44e75f99e30f5c7653f50a702bcc0977ae95dfe2
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-02 08:59:14 +0300

    lesson09 - add ObjectStreamStorage

--- src/com/dzvonik/webapp/storage/ObjectStreamStorage.java
+++ src/com/dzvonik/webapp/storage/ObjectStreamStorage.java
@@ -1,10 +1,13 @@
 package com.dzvonik.webapp.storage;
 
+import com.dzvonik.webapp.exception.StorageException;
 import com.dzvonik.webapp.model.Resume;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
@@ -23,7 +26,11 @@ public class ObjectStreamStorage extends AbstractFileStorage {
 
     @Override
     protected Resume doRead(InputStream is) throws IOException {
-        return null;
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return (Resume) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new StorageException("Error reading resume", null, e);
+        }
     }
 
 }
