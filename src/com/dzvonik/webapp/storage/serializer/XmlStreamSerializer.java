f9dd9bd5e62e90616ab45f9c9b2ddae4b4786ec7
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-09 19:31:58 +0300

    lesson10 - add XmlStreamSerializer

---
+++ src/com/dzvonik/webapp/storage/serializer/XmlStreamSerializer.java
@@ -0,0 +1,36 @@
+package com.dzvonik.webapp.storage.serializer;
+
+import com.dzvonik.webapp.model.Link;
+import com.dzvonik.webapp.model.ListSection;
+import com.dzvonik.webapp.model.Organization;
+import com.dzvonik.webapp.model.OrganizationSection;
+import com.dzvonik.webapp.model.Resume;
+import com.dzvonik.webapp.model.TextSection;
+import com.dzvonik.webapp.util.XmlParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class XmlStreamSerializer implements StreamSerializer {
+    private XmlParser xmlParser;
+
+    public XmlStreamSerializer() {
+        xmlParser = new XmlParser(
+                Resume.class, Organization.class, Link.class,
+                OrganizationSection.class, TextSection.class, ListSection.class, Organization.Position.class);
+    }
+
+    @Override
+    public void doWrite(Resume r, OutputStream os) throws IOException {
+        try (Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            xmlParser.marshall(r, w);
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return xmlParser.unmarshall(r);
+        }
+    }
+}
