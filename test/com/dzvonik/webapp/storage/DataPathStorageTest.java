cf81603c5e467912a2a5817337814779a459f4d2
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-12 07:40:04 +0300

    lesson10 - hw

---
+++ test/com/dzvonik/webapp/storage/DataPathStorageTest.java
@@ -0,0 +1,11 @@
+package com.dzvonik.webapp.storage;
+
+import com.dzvonik.webapp.storage.serializer.JsonStreamSerializer;
+
+public class JsonPathStorageTest extends AbstractStorageTest {
+
+    public JsonPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new JsonStreamSerializer()));
+    }
+
+}
