22ea61f32ab7979729e8ca57091098f575aede74
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-15 06:20:24 +0300

    lesson11 - synchronized

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,13 +1,31 @@
 public class MainConcurrency {
 
-    public static void main(String[] args) {
-        Runnable runnable = () -> {
-            System.out.println("New thread is start!");
-        };
+    private static final Object LOCK = new Object();
+    private static int counter = 0;
 
-        Thread thread1 = new Thread(runnable);
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Name: " + Thread.currentThread().getName() + ", State: " + Thread.currentThread().getState());
+        });
         thread1.start();
-        System.out.println(thread1.getName());
+
+        for (int i = 0; i < 10000; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    inc(LOCK);
+                }
+            }).start();
+        }
+
+        Thread.sleep(5000);
+        System.out.println(counter);
+    }
+
+    private static synchronized void inc(Object LOCK) {
+        double tmp = Math.sin(13.);
+        synchronized (LOCK) {
+            counter++;
+        }
     }
 
 }
