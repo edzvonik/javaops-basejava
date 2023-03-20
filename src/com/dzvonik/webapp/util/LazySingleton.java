a9b8df64f1d44ab16b349901b838bc7d4c850cf9
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-20 12:38:22 +0300

    lesson11 - add LazySingleton

--- src/com/dzvonik/webapp/util/LazySingleton.java
+++ src/com/dzvonik/webapp/util/LazySingleton.java
@@ -2,12 +2,15 @@ package com.dzvonik.webapp.util;
 
 public final class LazySingleton {
 
-    private static final LazySingleton INSTANCE = new LazySingleton();
+    private static LazySingleton INSTANCE;
 
     private LazySingleton() {
     }
 
     public static LazySingleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new LazySingleton();
+        }
         return INSTANCE;
     }
 
