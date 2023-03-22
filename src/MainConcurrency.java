4ec55751d0fdeec2fbd7b719b3dc87b26aa10853
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-22 12:29:32 +0300

    lesson11 - add deadlock

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,31 +1,95 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainConcurrency {
 
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
     private static final Object LOCK = new Object();
-    private static int counter = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(() -> {
-            System.out.println("Name: " + Thread.currentThread().getName() + ", State: " + Thread.currentThread().getState());
-        });
-        thread1.start();
+        System.out.println(Thread.currentThread().getName());
 
-        for (int i = 0; i < 10000; i++) {
-            new Thread(() -> {
-                for (int j = 0; j < 100; j++) {
-                    inc(LOCK);
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
                 }
-            }).start();
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
         }
 
-        Thread.sleep(5000);
-        System.out.println(counter);
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(mainConcurrency.counter);
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+
     }
 
-    private static synchronized void inc(Object LOCK) {
-        double tmp = Math.sin(13.);
-        synchronized (LOCK) {
-            counter++;
-        }
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
+    private synchronized void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+        counter++;
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
 }
