be7ea05f7e5547ba11575dea66efbae55c9d8ae6
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-03-23 23:13:25 +0300

    -

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,7 +1,5 @@
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -125,7 +123,6 @@ public class MainConcurrency {
 //        }
 //                wait();
 //                readFile
-//                ...
 //        }
     }
 
